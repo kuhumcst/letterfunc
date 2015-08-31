03e1451c8682941200f619ae4b51626ef3d2ee82
Bart Jongejan <bart@cst.dk>
2015-08-31 12:31:22 +0200

    Replaced tab by four spaces

--- src/letterfunc.cpp
+++ src/letterfunc.cpp
@@ -34,7 +34,7 @@ bool isAlpha(int kar)
         return true;*/
 
 /* From http://www.w3.org/TR/xml11/#NT-NameStartChar:
-NameStartChar	   ::=   	":" | [A-Z] | "_" | [a-z] | [#xC0-#xD6] 
+NameStartChar       ::=       ":" | [A-Z] | "_" | [a-z] | [#xC0-#xD6] 
         | [#xD8-#xF6] | [#xF8-#x2FF] | [#x370-#x37D] | [#x37F-#x1FFF] 
         | [#x200C-#x200D] | [#x2070-#x218F] | [#x2C00-#x2FEF] 
         | [#x3001-#xD7FF] | [#xF900-#xFDCF] | [#xFDF0-#xFFFD] 
